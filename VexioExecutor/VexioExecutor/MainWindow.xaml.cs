521bc3354d12294cc615a40d510fdf881b5154f9
NikkkCoding <125396298+NikkkCoding@users.noreply.github.com>
2023-04-15 21:31:08 +0200

    Add files via upload

---
+++ VexioExecutor/VexioExecutor/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace VexioExecutor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Stream stream = File.OpenRead("./bin/lua.xshd");
+            XmlTextReader reader = new XmlTextReader(stream);
+            Avalon.SyntaxHighlighting = HighlightingLoader.Load(reader, HighlightingManager.Instance);
+
+
+        }
+
+        private void MainBorder_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            Environment.Exit(0);
+        }
+    }
+}
